bbe16fd531c84988cf322da59366754a2b783d03
Niamh <niamh.gowran2@mail.dcu.ie>
2023-03-03 00:13:36 +0000

    Homepage route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
+  /*
+  // set the favicon to the icon in the assets folder
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.type = "image/x-icon";
+    link.rel = "shortcut icon";
+    link.href = icon;
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }, []);
+
+  // set the manifest to the manifest in the assets folder
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "manifest";
+    link.href = "/manifest.json";
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }, []);*/
+
   return (
-    <div>
-      <h1 className="font-bold">React App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} exact />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
