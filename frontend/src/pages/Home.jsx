eb7aa9c179bb33877f1254b0d0e366c2a7d05bf6
CheezyHint <samchoa11@gmail.com>
2023-03-09 17:42:28 +0000

    Footer Created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 const Home = () => {
-    return (
-        <div>
-            {/* This div may be moved to the nav component in future */}
-            <div className="mx-auto pt-16">
-                <img src="/oftc.jpg" alt="otrc logo" className="w-52 h-52 mx-auto rounded-full" />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      {/* This div may be moved to the nav component in future */}
+      <div className="mx-auto pt-16">
+        <img
+          src="/oftc.jpg"
+          alt="otrc logo"
+          className="w-52 h-52 mx-auto rounded-full"
+        />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
